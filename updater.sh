1fa2e10783be2ac99db49eb202ff9017c2465a81
Rémi <remim.dupre@gmail.com>
2014-10-12 19:31:06 +0200

    Update de l'updater

--- updater.sh
+++ updater.sh
@@ -7,12 +7,10 @@ rm -R Indept-master/comptes Indept-master/fichiers
 
 if [ ! -d "dept/comptes" ]; then
     mkdir Indept-master/comptes
-    chown http:http Indept-master/comptes
     echo "Le dossier \"comptes\" a dut etre cree, verifiez les permissions"
 fi
 if [ ! -d "dept/fichiers" ]; then
     mkdir Indept-master/fichiers
-    chown http:http Indept-master/fichiers
     echo "Le dossier \"fichier\" a dut etre cree, verifiez les permissions"
 fi
 cp -R dept/comptes Indept-master/comptes
@@ -21,4 +19,5 @@ cp -R dept/fichiers Indept-master/fichiers
 rm master.zip
 rm -R dept
 mv Indept-master dept
+chown -R http:http dept
 mv dept/updater.sh updater.sh

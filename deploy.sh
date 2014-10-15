5da87afac853f8cac66c366a637d11197c61afda
Remi <remim.dupre@gmail.com>
2014-10-15 04:23:40 +0000

    Debug travis

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,9 @@ grunt
 mv web indept
 rm -r indept/css/src indept/js/src
 
+echo "Création fichier .zip"
 zip -9r indept-build indept/
+echo "Création fichier .tar.gz"
 tar -vczf indept-build.tar.gz indept/
+
+ls

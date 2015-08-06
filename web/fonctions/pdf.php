b0b00471a4e9ae990d674a8ed82c6e7e5abc8cc7
Remi <remim.dupre@gmail.com>
2015-08-06 15:20:00 +0000

    Meilleur récapitulatif

--- web/fonctions/pdf.php
+++ web/fonctions/pdf.php
@@ -1,5 +1,6 @@
 <?php
     require_once("../php/fichiers.php");
+    require_once("../php/latex.php");
     session_start();
     setlocale(LC_TIME, "fr_FR");
     
@@ -25,74 +26,4 @@
     @readfile($file);
     
     exec("rm facture$id.*");
-    
-    function recap($json) {
-        $nom_fichier = $json['nom'];
-        $date_modif = date('d/m/Y', $json['derniere_edition']);
-        return "
-            \\begin{itemize}
-               \\item Nom du fichier : \\textbf{ $nom_fichier }
-               \\item Dernière édition : \\textbf{ $date_modif }
-            \\end{itemize}
-        ";
-    }
-    
-    function detail($json) {
-        $retour = '';
-        $liste_mois = array();
-        foreach( $json['liste'] as $i => $ligne ) {
-            $mois = mktime(1, 1, 0, date("n", $ligne['date']), 1, date("Y", $ligne['date']) );
-            if( !isset($liste_mois[$mois]) ) $liste_mois[$mois] = array();
-            array_push($liste_mois[$mois], $ligne);
-        }
-        uksort($liste_mois);
-        
-        foreach( $liste_mois as $mois => $lignes ) {
-            usort($lignes, "ordreLignes");
-            $retour .= '\\paragraph{ ' . date("F Y", $mois) . ' }' . detailListe($lignes);
-        }
-        
-        return $retour;
-    }
-    
-    function detailListe($liste) {
-        $lignes = '';
-        $depense = 0;
-        $encaisse = 0;
-        $nb_lignes = count($liste);
-        
-        foreach( $liste as $i => $ligne ) {
-            $lignes .= ligneTableau($ligne);
-            if( $ligne['montant'] > 0 ) $depense += $ligne['montant'];
-            else $encaisse -= $ligne['montant'];
-        }
-        
-        return "
-            Dépensé : $depense €, Encaissé : $encaisse €, Entrées : $nb_lignes ~~\\\\~~\\\\
-            \\begin{tabularx}{\\textwidth}{| l | c | c | X |}
-                \\hline $lignes
-            \\end{tabularx}
-        ";
-    }
-    
-    function ligneTableau($ligne) {
-        $nom = tex_format( $ligne['titre'] );
-        $montant = $ligne['montant'];
-        $date = date('d/m/Y', $ligne['date']);
-        $description = tex_format( $ligne['description'] );
-        return " $nom & $montant € & $date & $description \\\\ \\hline ";
-    }
-    
-    function ordreLignes($ligneA, $ligneB) {
-        return $ligneA['date'] - $ligneB['date'];
-    }
-    
-    function ordreMois($listeA, $listeB) {
-        return $listeA[0]['date'] - $listeB[0]['date'];
-    }
-    
-    function tex_format($str) {
-        $str = str_replace('&', '\&', $str);
-        return $str;
-    }
 ?>
